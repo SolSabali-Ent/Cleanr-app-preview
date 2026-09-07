91beecebcc6967d63764a236c94723fb5fb0a1f0
Cleanr Public Publisher <225978092+SolSabaliEnt@users.noreply.github.com>
2026-09-07 00:08:52 +0000

    Sync Cleanr public app from 548e453

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -51,6 +51,7 @@ import CSPSignup from "../app/provider/screens/CSPSignup";
 import GeoHarness from "../screens/admin/GeoHarness";
 import { AdminAccess } from "../screens/admin/AdminAccess";
 import { AdminAffiliateCashouts } from "../screens/admin/AdminAffiliateCashouts";
+import { AdminCircles } from "../screens/admin/AdminCircles";
 import { AdminCollectiveCapacity } from "../screens/admin/AdminCollectiveCapacity";
 import { AdminGrowthCirculation } from "../screens/admin/AdminGrowthCirculation";
 import { AdminBookingMessagesScreen } from "../screens/admin/AdminBookingMessagesScreen";
@@ -216,6 +217,7 @@ export function AppRouter() {
             <Route index element={<Navigate to="/admin/ops" replace />} />
             <Route path="geo" element={<GeoHarness />} />
             <Route path="ops" element={<OperationsDashboard />} />
+            <Route path="circles" element={<AdminCircles />} />
             <Route path="collective-capacity" element={<AdminCollectiveCapacity />} />
             <Route path="opportunity-circulation" element={<AdminGrowthCirculation />} />
             <Route path="affiliate-cashouts" element={<AdminAffiliateCashouts />} />
